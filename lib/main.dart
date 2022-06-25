8dc179bc652b8bf66012b572d6498ce1cb00ada4
chesnoksatan <chestwins@gmail.com>
2022-06-25 21:40:26 +0300

    Main: remove Browser widget
    Move Browser widget into separated file
    Change ThemeData ( use from files repo )

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:web_browser/searchbar.dart';
+import 'package:provider/provider.dart';
+import 'package:web_browser/backend/tab_contoller.dart';
+import 'package:web_browser/browser.dart';
 
 void main() => runApp(BrowserApp());
 
@@ -24,217 +25,35 @@ class BrowserApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Terminal',
-      theme: new ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.red,
-        primaryColor: const Color(0xFF212121),
-        canvasColor: const Color(0xFF303030),
-      ),
-      home: new Browser(),
-    );
-  }
-}
-
-//double iconSize = null;
-
-class Desktop extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        height: 50,
-        color: Color(0xffF7F7F7),
-        child: new Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              new IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {},
-                //iconSize: iconSize,
-                color: const Color(0xFF000000),
-              ),
-              new IconButton(
-                icon: const Icon(Icons.arrow_forward),
-                onPressed: () {},
-                //iconSize: iconSize,
-                color: const Color(0xFF000000),
-              ),
-              new IconButton(
-                icon: const Icon(Icons.refresh),
-                onPressed: () {},
-                //  iconSize: iconSize,
-                color: const Color(0xFF000000),
-              ),
-              new SearchWidget(),
-              new IconButton(
-                icon: const Icon(Icons.star_border),
-                onPressed: () {},
-                //  iconSize: iconSize,
-                color: const Color(0xFF000000),
-              ),
-              new IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {},
-                //  iconSize: iconSize,
-                color: const Color(0xFF000000),
-              ),
-            ]),
-      ),
-    ]);
-  }
-}
-
-class Browser extends StatefulWidget {
-  Browser({Key? key}) : super(key: key);
-  @override
-  BrowserState createState() => new BrowserState();
-}
-
-class BrowserState extends State<Browser> with TickerProviderStateMixin {
-  List<Tab> tabs = [];
-  late TabController tabController;
-  var count = 1;
-  void newTab() {
-    setState(() {
-      tabs.add(
-        Tab(
-          child: Row(
-            children: <Widget>[
-              Text('New Tab'),
-              Padding(
-                padding: EdgeInsets.only(left: 8),
-              ),
-              new Expanded(child: new Container()),
-              GestureDetector(
-                child: Icon(
-                  Icons.clear,
-                  size: 16,
-                  //color: Colors.black,
-                ),
-                onTap: closeCurrentTab,
-              ),
-            ],
-          ),
+      title: 'Browser',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark(
+          primary: Colors.deepOrange,
+          primaryVariant: Colors.deepOrange,
+          secondary: Colors.deepOrange,
+          secondaryVariant: Colors.deepOrange,
+          background: Color(0xFF161616),
+          surface: Color(0xFF212121),
+          onPrimary: Colors.white,
+          onSecondary: Colors.white,
+          onBackground: Colors.white,
+          onSurface: Colors.white,
+          onError: Colors.white,
         ),
-      );
-      count++;
-      tabController = TabController(length: tabs.length, vsync: this);
-    });
-  }
-
-  void closeCurrentTab() {
-    setState(() {
-      tabs.removeAt(tabController.index);
-      tabController = TabController(length: tabs.length, vsync: this);
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    tabs.add(
-      Tab(
-        child: Row(
-          children: <Widget>[
-            Text('New Tab'),
-            Padding(
-              padding: EdgeInsets.only(left: 8),
-            ),
-            new Expanded(child: new Container()),
-            GestureDetector(
-              child: Icon(
-                Icons.clear,
-                size: 16,
-                //color: Colors.black,
-              ),
-              onTap: closeCurrentTab,
-            ),
-          ],
+        scrollbarTheme: ScrollbarThemeData(
+          isAlwaysShown: true,
+          showTrackOnHover: true,
+          trackBorderColor: MaterialStateProperty.all(Colors.transparent),
+          crossAxisMargin: 0,
+          mainAxisMargin: 0,
+          radius: Radius.zero,
         ),
       ),
-    );
-    tabController = TabController(length: tabs.length, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          backgroundColor: Colors.blue,
-          appBar: PreferredSize(
-              preferredSize: Size.fromHeight(
-                  55.0), // here the desired height of the status bar
-              child: AppBar(
-                  elevation: 0.0,
-                  backgroundColor: const Color(0xFFD6D6D6),
-                  bottom: PreferredSize(
-                      preferredSize: Size.fromHeight(
-                          55.0), // here the desired height of the status bar
-                      child: new Row(
-                        children: [
-                          new Expanded(
-                              child: new Container(
-                            child: TabBar(
-                                controller: tabController,
-                                labelColor: Color(0xFF222222),
-                                unselectedLabelColor: Color(0xFF222222),
-                                indicator: BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(5),
-                                        topRight: Radius.circular(5)),
-                                    color: Color(0xFFF7F7F7)),
-                                tabs: tabs.map((tab) => tab).toList()),
-                          )),
-                          new Center(
-                            child: new IconButton(
-                                icon: Icon(Icons.add),
-                                color: Color(0xFF222222),
-                                onPressed: newTab),
-                          ),
-                          /* new Center(
-                            child: new Container(height:20,width:1,color:Color(0xFF222222),)
-                          ),
-                          new Center(
-                            child: new IconButton(
-                                icon: Icon(Icons.more_vert),
-                                color: Color(0xFF222222),
-                                onPressed: newTab),
-                          )*/
-                        ],
-                      )) // A trick to trigger TabBar rebuild.
-                  )),
-          body: Stack(
-            children: [
-              new Center(
-                child: new Text(
-                  "?",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              TabBarView(
-                controller: tabController,
-                children: tabs
-                    .map((tab) => Column(
-                          children: [
-                            new Desktop(),
-                            new Expanded(
-                              child: Text("EMPTY"),
-                            )
-                          ],
-                        ))
-                    .toList(),
-              ),
-            ],
-          )),
+      debugShowCheckedModeBanner: false,
+      home: ChangeNotifierProvider(
+        create: (_) => BrowserTabController(),
+        builder: (context, child) => const Browser(),
+      ),
     );
   }
 }
